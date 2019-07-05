a3e4e58b63cd30a46a4b1195af2ef66680f19819
tecnelm <cleman91@gmail.com>
2019-07-05 23:52:56 +0200

    compute ok blocage du nombre de nombre ok TO DO
    mettre résultat en float
    ajuster la taille du nombre

--- app/src/main/java/ovh/garrigues/android/calculator/MainActivity.java
+++ app/src/main/java/ovh/garrigues/android/calculator/MainActivity.java
@@ -12,108 +12,133 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Long> numberList = new ArrayList<>();
     private ArrayList<Operator> operandList = new ArrayList<>();
-    private TextView operationView ;
-    private TextView resultView ;
+    private TextView operationView;
+    private TextView resultView;
     private long actualNumber = 0;
     private boolean calculed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         operationView = findViewById(R.id.OperationScreen);
-         resultView = findViewById(R.id.resultScreen);
+        operationView = findViewById(R.id.OperationScreen);
+        resultView = findViewById(R.id.resultScreen);
     }
 
     public void addoperand(View view) {
         Operator op;
-        switch (view.getId())
-        {
-            case R.id.ButtonPlus: op = Operator.PLUS ;break;
-            case R.id.ButtonDivide:op = Operator.DIVIDE ; break;
-            case R.id.ButtonMinud:op = Operator.MINUS ; break;
-            case R.id.ButtonMult:op = Operator.MULT ; break;
-            default: op=null;
+        switch (view.getId()) {
+            case R.id.ButtonPlus:
+                op = Operator.PLUS;
+                break;
+            case R.id.ButtonDivide:
+                op = Operator.DIVIDE;
+                break;
+            case R.id.ButtonMinud:
+                op = Operator.MINUS;
+                break;
+            case R.id.ButtonMult:
+                op = Operator.MULT;
+                break;
+            default:
+                op = null;
         }
         operandList.add(op);
         numberList.add(actualNumber);
-        affScreen(op,actualNumber);
-        actualNumber=0;
+        affScreen(op, actualNumber);
+        actualNumber = 0;
         resultView.setText(String.valueOf(actualNumber));
 
     }
 
     public void compute(View view) {
-        long result ;
+        long result;
         numberList.add(actualNumber);
 
-        if (numberList.size() >0)
-        {   result = numberList.get(0);
+        if (numberList.size() > 0) {
+            result = numberList.get(0);
 
-            for(int index =0; index < operandList.size() ; index ++)
-            {
-                Operator op  = operandList.get(index);
-                long number_to_add ;
-                if (index == numberList.size()-1)
+            for (int index = 0; index < operandList.size(); index++) {
+                Operator op = operandList.get(index);
+                long number_to_add;
+                if (index == numberList.size() - 1)
                     number_to_add = 0;
-                else
-                {
-                    number_to_add = numberList.get(index+1);
+                else {
+                    number_to_add = numberList.get(index + 1);
                 }
-                if(op == Operator.DIVIDE && number_to_add == 0)
-                {
+                if (op == Operator.DIVIDE && number_to_add == 0) {
                     resultView.setText("ERROR");
-                }
-                else
-                {
-                    switch (op)
-                    {
-                        case MULT:result *= number_to_add;break;
-                        case PLUS: result +=number_to_add;break;
-                        case MINUS:result -=number_to_add;break;
-                        case DIVIDE:result /=number_to_add;break;
+                } else {
+                    switch (op) {
+                        case MULT:
+                            result *= number_to_add;
+                            break;
+                        case PLUS:
+                            result += number_to_add;
+                            break;
+                        case MINUS:
+                            result -= number_to_add;
+                            break;
+                        case DIVIDE:
+                            result /= number_to_add;
+                            break;
                     }
 
                 }
             }
-        }
-        else
-        {
+        } else {
             result = actualNumber;
         }
         resultView.setText(String.valueOf(result));
+        affScreen(null, actualNumber);
+        operandList.clear();
+        numberList.clear();
+        actualNumber = 0;
         calculed = true;
     }
 
     public void addnumber(View view) {
-        Long num= Long.parseLong(((Button)view).getText().toString());
-       actualNumber*=10;
-       actualNumber+=num;
-       resultView.setText(""+actualNumber);
-    }
-    private void affScreen(Operator operator,long number)
-    {
-        String str =operationView.getText().toString();
-        String op = "" ;
-        switch (operator)
-        {
-            case MULT:op="*";break;
-            case MINUS:op="-";break;
-            case PLUS:op="+";break;
-            case DIVIDE:op="/";break;
+        if (resultView.getText().length() <= 9) {
+            long num = Long.parseLong(((Button) view).getText().toString());
+            actualNumber *= 10;
+            actualNumber += num;
+            resultView.setText("" + actualNumber);
         }
-        if (numberList.size() == 1)
-            operationView.setText(""+number+" "+op);
-        else
-            operationView.setText( str+" "+number+" "+op);
-        if (calculed)
-        {
-            operationView.setText("0");
+    }
+
+    private void affScreen(Operator operator, long number) {
+        if (calculed) {
+            operationView.setText("");
             calculed = false;
+        }
+        String str = operationView.getText().toString();
+        String op = "";
+        if (operator != null) {
+            switch (operator) {
+                case MULT:
+                    op = "*";
+                    break;
+                case MINUS:
+                    op = "-";
+                    break;
+                case PLUS:
+                    op = "+";
+                    break;
+                case DIVIDE:
+                    op = "/";
+                    break;
+
+            }
+        }
+        if (numberList.size() == 1) {
+            operationView.setText("" + number + " " + op);
+        } else {
+            operationView.setText(str + " " + number + " " + op);
         }
 
     }
-    private enum Operator
-    {
+
+    private enum Operator {
         PLUS,
         MINUS,
         MULT,
